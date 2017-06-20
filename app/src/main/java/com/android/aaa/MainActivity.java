3b9b4300c371e1371e9a23122b36acc510b321a3
habee1 <javyy.x41@gmail.com>
2017-06-19 22:57:49 -0400

    Initial commit

---
+++ app/src/main/java/com/android/aaa/MainActivity.java
@@ -0,0 +1,53 @@
+package com.android.aaa;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity{
+
+    private AdView AdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        AdView = (AdView)findViewById(R.id.anuncio_view);
+        final AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        AdView.loadAd(adRequest);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(AdView != null){
+            AdView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (AdView != null){
+            AdView.resume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(AdView != null){
+            AdView.pause();
+        }
+        super.onPause();
+    }
+
+}
